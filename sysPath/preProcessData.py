c11846fb9493e4562d0129f6b9dfe82e2a6593ca
Mohamed Erfan Mohamed Elfares <MohamedErfan1111@gmail.com>
2023-09-30 11:04:18 +0400

    .

--- sysPath/preProcessData.py
+++ sysPath/preProcessData.py
@@ -1,34 +1,15 @@
-import os
 import re
 import nltk
-import shutil
 import warnings
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 import pyarabic.normalize as Normalize
 
-from nltk import ngrams
 from nltk.corpus import stopwords
 from nltk.stem import ISRIStemmer
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import word_tokenize
 
-from keras.callbacks import TensorBoard
-from keras.preprocessing.text import Tokenizer
-from keras.utils import plot_model, pad_sequences
+from sklearn.preprocessing import LabelEncoder
 
-from sklearn.svm import SVC
-from sklearn.decomposition import PCA
-from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
-from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score, classification_report, confusion_matrix, ConfusionMatrixDisplay
-
-from numpy import array
 from matplotlib import style
-from gensim.models import KeyedVectors
-from transformers import MarianMTModel, MarianTokenizer
 
 le = LabelEncoder()
 style.use("ggplot")
@@ -39,13 +20,6 @@ style.use("ggplot")
 # nltk.download('stopwords') # download stopwords if not already downloaded
 # nltk.download('averaged_perceptron_tagger')
 warnings.filterwarnings(action = 'ignore')
-
-
-
-path = r"C:\Users\Mohamed\Documents\Fall 2023 - 2024\Senior Project in CS\SVC"
-if os.path.isdir("logs"):
-    shutil.rmtree("logs")
-callback = TensorBoard(log_dir=rf'{path}\logs/', histogram_freq=1)
 
 
 
@@ -172,97 +146,3 @@ def preProcessData(dataset):
     print("\n----------     dataTokenization Done!      ----------\n")
 
     return data
-
-
-
-dataset = pd.read_csv(r"C:\Users\Mohamed\Documents\Fall 2023 - 2024\Senior Project in CS\Total Dataset.csv")
-dataset.info()
-# display the dataset before the pre-processing
-print(f"\n{dataset.head()}")
-
-
-
-cleaned_dataset = preProcessData(dataset.copy(deep=True))
-# cleaned_dataset.to_excel('cleanedset.xlsx', index=False)
-
-
-
-# prepare tokenizer
-T = Tokenizer()
-T.fit_on_texts(cleaned_dataset["tweet"].tolist())
-vocab_size = len(T.word_index) + 1
-
-
-
-# integer encode the documents
-encoded_docs = T.texts_to_sequences(cleaned_dataset["tweet"].tolist())
-# print("encoded_docs:\n",encoded_docs)
-
-
-
-# pad documents to a max length of 4 words
-max_length = len(max(np.array(cleaned_dataset["tweet"]), key=len))
-padded_docs = pad_sequences(encoded_docs, maxlen = max_length, padding = "post")
-print("\npadded_docs:\n\n",padded_docs)
-
-
-
-columns = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"]
-# columns = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-# columns = ["A", "B", "C", "D", "E"]
-padded_docs = PCA(n_components=len(columns)).fit_transform(padded_docs)
-padded_docs = StandardScaler().fit_transform(padded_docs)
-padded_docs = MinMaxScaler().fit_transform(padded_docs)
-
-features = pd.DataFrame(padded_docs, columns=columns)
-features["sentiment"] = cleaned_dataset[["sentiment"]].copy()
-features["dialect"] = cleaned_dataset[["dialect"]].copy()
-
-# features = PCA(n_components=5).fit_transform(features)
-# features = StandardScaler().fit_transform(features)
-# features = MinMaxScaler((-1, 1)).fit_transform(features)
-labels = cleaned_dataset[["sarcasm"]].copy()
-
-
-
-# splits into traint & test
-tweet_train, tweet_test, labeled_train, labeled_test = train_test_split(features, labels.to_numpy(), test_size=0.2, shuffle=True)
-
-
-
-# fit the model
-svc = SVC(kernel="poly", C=1.0)
-svc.fit(tweet_train, labeled_train)
-
-
-
-#evaluate the model
-trainScore = svc.score(tweet_train, labeled_train)
-testScore = svc.score(tweet_test, labeled_test)
-labelPredicted = svc.predict(tweet_test)
-
-print()
-
-print(f"\nSVC on the training dataset: {trainScore:.2f}")
-print(f"SVC on the test dataset:     {testScore:.2f}\n")
-
-print(classification_report(labeled_test, labelPredicted, target_names=["Class: 0", "Class: 1"]))
-
-
-
-confusionMatrix = confusion_matrix(labeled_test, labelPredicted)
-
-ax= plt.subplot()
-sns.heatmap(confusionMatrix, annot=True, fmt='g', ax=ax, cmap="viridis") # annot=True to annotate cells, ftm='g' to disable scientific notation
-# sns.heatmap(confusionMatrix, annot=True, fmt='g', ax=ax, cmap="Blues") # annot=True to annotate cells, ftm='g' to disable scientific notation
-
-# labels, title and ticks
-ax.set_xlabel('Predicted labels')
-ax.set_ylabel('True labels')
-ax.set_title(f"Accuracy: {testScore*100:.2f}%")
-
-ax.xaxis.set_ticklabels([0, 1])
-ax.yaxis.set_ticklabels([0, 1])
-
-plt.savefig(r"C:\Users\Mohamed\Documents\Fall 2023 - 2024\Senior Project in CS\SVC\confusionMatrix.png", dpi=1000)
-plt.show()
