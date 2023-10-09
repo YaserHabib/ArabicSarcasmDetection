14a5732235ee61a09f7e67febdf6ba3e11121ada
Mohamed Erfan Mohamed Elfares <MohamedErfan1111@gmail.com>
2023-10-09 14:55:20 +0400

    updated

--- Machine Learning Models/KNN/model.py
+++ Machine Learning Models/KNN/model.py
@@ -3,7 +3,7 @@ import sys
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
-sys.path.append(r"C:\Users\Admin\OneDrive\Desktop\Project\ArabicSarcasmDetection\sysPath")
+sys.path.append(r"C:\Users\Mohamed\Documents\Fall 2023 - 2024\Senior Project in CS\sysPath")
 os.chdir(dname)
 
 import time
@@ -19,8 +19,8 @@ from keras.utils import pad_sequences
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
+from sklearn.metrics import classification_report, confusion_matrix, precision_score, recall_score, f1_score
 
 from matplotlib import style
 
@@ -34,14 +34,186 @@ if not os.path.isfile(r"Description.txt"):
 
 
 
+def featureEngineering(dataset, padded_docs, columns):
+    labels = dataset["sarcasm"]
+    dataset = dataset.drop(["sarcasm"], axis=1)
+
+    padded_docs = PCA(n_components=len(columns)).fit_transform(padded_docs)
+    padded_docs = StandardScaler().fit_transform(padded_docs)
+    padded_docs = MinMaxScaler().fit_transform(padded_docs)
+
+    features = pd.DataFrame(padded_docs, columns=columns)
+    for column in dataset.columns:
+        features[column] = dataset[column]
+
+    return features, labels
+
+
+
+def modelEvaluation(classifier, train, test):
+    trainScore = classifier.score(train["data"], train["labels"])
+    testScore = classifier.score(test["data"], test["labels"])
+    labelPredicted = classifier.predict(test["data"])
+
+    return trainScore, testScore, labelPredicted
+
+
+
+def display(variables, dimentionReduction, datasetName, trainScore, testScore, classificationReport, vars, startTime):
+    endTime = time.time()
+    vars.sort()
+
+    print("="*100)
+    for var in vars: print(f"{var} used:\t{variables[var]}")
+    print(f"Dimention Reduction:  {dimentionReduction}")
+    print(f"Dataset used: {datasetName}")
+    print(f"\nSVC model score on the training dataset:  {trainScore:.2f}")
+    print(f"SVC model score on the test dataset:      {testScore:.2f}")
+    print(f"Execution Time:   {abs(endTime - startTime)}s\n")
+    print(classificationReport) # type: ignore
+    print("\n")
+
+
+
+
+def recordResult(variables, dimentionReduction, datasetName, trainScore, testScore, classificationReport, vars, startTime):
+    endTime = time.time()
+    vars.sort()
+    
+    with open(r"Description.txt", "a") as descriptionFile:
+        descriptionFile.write("="*100)
+    descriptionFile.close()
+
+    with open(r"Description.txt", "a") as descriptionFile:
+        for var in vars:
+            descriptionFile.write(f"\n{var} used:\t{variables[var]}")
+        descriptionFile.close()
+
+    with open(r"Description.txt", "a") as descriptionFile:
+        descriptionFile.write(f"\nDimention Reduction:  {dimentionReduction}")
+        descriptionFile.write(f"\nDataset used: {datasetName}\n")
+        descriptionFile.write(f"\nSVC model score on the training dataset:  {trainScore:.2f}")
+        descriptionFile.write(f"\nSVC model score on the test dataset:      {testScore:.2f}")
+        descriptionFile.write(f"\nExecution Time:   {abs(endTime - startTime)}s\n\n")
+        descriptionFile.write(classificationReport) # type: ignore
+        descriptionFile.write("\n\n")
+        descriptionFile.close()
+
+
+
+def saveFig(datasetName, variables, DR, true, predicted, testScore):
+    confusionMatrix = confusion_matrix(true, predicted)
+    ax= plt.subplot()
+    sns.heatmap(confusionMatrix, annot=True, fmt='g', ax=ax, cmap="viridis") # annot=True to annotate cells, ftm='g' to disable scientific notation
+
+    # labels, title and ticks
+    ax.set_xlabel('Predicted labels')
+    ax.set_ylabel('True labels')
+    ax.set_title(f"Accuracy: {testScore*100:.2f}%")
+
+    ax.xaxis.set_ticklabels(["0", "1"])
+    ax.yaxis.set_ticklabels(["0", "1"])
+    variables = str(variables).replace('\'', '').replace(':', '=').replace(', ', " - ")
+    
+    plt.savefig(f"{datasetName} - {variables} - {len(DR)}DR.png", dpi=1000)
+    plt.close()
+
+
+def recordXLSX(data, DR, classifierVariables, precision, recall, f1, testScore):
+    weights = classifierVariables["weights"]
+
+    data.loc[len(data.index)] = [DR, weights, precision, recall, f1, testScore] # type: ignore
+
+
+
+def barBlot(DR, data):
+    variables = ["weights"]
+    weights = data[data.DR == DR]["weights"].tolist()
+
+    names = list()
+    for index in range(len(weights)):
+        names.append(weights[index])
+
+    f1 = data[data.DR == DR]["F1-score"]
+    accuracy = data[data.DR == DR]["Accuracy"]
+    measures = pd.concat([f1, accuracy], axis=1, join='inner')
+
+    x = np.arange(len(names))  # the label locations
+    width = 0.25  # the width of the bars
+    multiplier = 0
+
+    fig, ax = plt.subplots(figsize =(15, 10))
+
+    for attribute, measurement in measures.items():
+        offset = width * multiplier
+        rects = ax.bar(x + offset, measurement, width, label=attribute)
+        ax.bar_label(rects, padding=3)
+        multiplier += 1
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel("percentage (%)")
+    ax.set_xlabel(f"Variables: {variables}")
+    ax.set_title("visualize the Impact of variables")
+
+    ax.set_xticks(x + (width/2), names)
+
+    ax.legend(loc="upper left", ncols=3)
+    ax.set_ylim(0, 1)
+    
+    plt.savefig(f"{DR}DR.png", dpi=1000)
+    plt.close()
+
+
+
+def MLprocess(dataset, padded_docs, columns, classifierVariables, datasetName, vars, Ctime):
+    dataset = dataset.drop(["tweet"], axis=1)
+    data = pd.DataFrame(columns=["DR", "weights", "Precision-score", "Recall-score", "F1-score", "Accuracy"])
+    startTime = time.time() - Ctime
+
+    for column in columns:
+        features, labels = featureEngineering(dataset, padded_docs, columns[column])
+        tweet_train, tweet_test, labeled_train, labeled_test = train_test_split(features.to_numpy(), labels.to_numpy(), test_size=0.2, shuffle=True)
+
+        for row in classifierVariables:
+            preTime = time.time() + startTime
+            weights = classifierVariables[row]["weights"]
+
+
+            knn = KNeighborsClassifier(weights=weights)
+            knn.fit(tweet_train, labeled_train)
+
+            test = {"data": tweet_test, "labels": labeled_test}
+            train = {"data": tweet_train, "labels": labeled_train}
+
+            trainScore, testScore, labelPredicted = modelEvaluation(knn, train, test)
+            classifierVars = {"weights": weights}
+            classificationReport = classification_report(labeled_test, labelPredicted, target_names=["Class: 0", "Class: 1"])
+
+            display(classifierVars, len(features.columns), datasetName, trainScore, testScore, classificationReport, vars, preTime)
+            recordResult(classifierVars, len(features.columns), datasetName, trainScore, testScore, classificationReport, vars, preTime)
+            saveFig(datasetName, classifierVars, features.columns, labeled_test, labelPredicted, testScore)
+
+            f1 = f1_score(labeled_test, labelPredicted)
+            recall = recall_score(labeled_test, labelPredicted)
+            precision = precision_score(labeled_test, labelPredicted)
+            recordXLSX(data, len(features.columns), classifierVars, precision, recall, f1, testScore)
+
+        barBlot(len(features.columns), data.copy(deep=True))
+
+    data.to_excel("record.xlsx", index=False)
+
+
+
 # dataset, datasetName = pd.read_csv(r"https://raw.githubusercontent.com/iabufarha/ArSarcasm-v2/main/ArSarcasm-v2/training_data.csv"), "Original Dataset"
-# dataset, datasetName = pd.read_csv(r"../../../Datasets/GPT Dataset.csv"), "GPT Combined Dataset"
+dataset, datasetName = pd.read_csv(r"../../Datasets/GPT Dataset.csv"), "GPT Combined Dataset"
 # dataset, datasetName = pd.read_csv(r"../../../Datasets/full Dataset.csv"), "Full Combined Dataset"
-dataset, datasetName = pd.read_csv(r"../../Datasets/augmented Dataset.csv"), "Augmented Combined Dataset"
+# dataset, datasetName = pd.read_csv(r"../../Datasets/augmented Dataset.csv"), "Augmented Combined Dataset"
 # dataset, datasetName = pd.read_csv(r"../../../Datasets/backtrans Dataset.csv"), "Back Translated Combined Dataset"
 # dataset, datasetName = pd.read_csv(r"../../../Datasets/synrep Dataset.csv"), "Synonym Replacement Combined Dataset"
 # dataset, datasetName = pd.read_csv(r"../../../Datasets/backGPT Dataset.csv"), "Back Translated & GPT Combined Dataset"
 # dataset, datasetName = pd.read_csv(r"../../../Datasets/synGPT Dataset.csv"), "Synonym Replacement & GPT Combined Dataset"
+
+
 
 dataset.info()
 print(f"\n{dataset.head()}")
@@ -68,91 +240,21 @@ encoded_docs = T.texts_to_sequences(cleaned_dataset["tweet"].tolist())
 # pad documents to a max length of 4 words
 max_length = len(max(np.array(cleaned_dataset["tweet"]), key=len))
 padded_docs = pad_sequences(encoded_docs, maxlen = max_length, padding = "post")
-print("\npadded_docs:\n\n",padded_docs)
+print("\npadded_docs:\n\n", padded_docs, "\n")
 
 
 
-columns = ["A", "B", "C", "D", "E"]
-#columns = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-# columns = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"]
-padded_docs = PCA(n_components=len(columns)).fit_transform(padded_docs)
-padded_docs = StandardScaler().fit_transform(padded_docs)
-padded_docs = MinMaxScaler().fit_transform(padded_docs)
-
-features = pd.DataFrame(padded_docs, columns=columns)
-features["sentiment"] = cleaned_dataset[["sentiment"]].copy()
-features["dialect"] = cleaned_dataset[["dialect"]].copy()
-
-labels = cleaned_dataset[["sarcasm"]].copy()
-
-
-
-# splits into traint & test
-tweet_train, tweet_test, labeled_train, labeled_test = train_test_split(features.to_numpy(), labels.to_numpy(), test_size=0.2, shuffle=True)
-
+columnsA = ["A", "B", "C", "D", "E"]
+columnsB = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+columnsC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"]
+columns = {"columnsA": columnsA, "columnsB": columnsB, "columnsC": columnsC}
 
 
 # fit the model
-#n_neighbors=3
-#n_neighbors,leaf_size,metric,algorithm=100,30,"minkowski","ball_tree"
-#n_neighbors,leaf_size,metric,algorithm=100,30,"manhattan","ball_tree"
-#n_neighbors,leaf_size,metric,algorithm=100,30,"euclidean","ball_tree"
-#n_neighbors,leaf_size,metric,algorithm=100,30,"euclidean","kd_tree"
-#n_neighbors,leaf_size,metric,algorithm=100,30,"manhattan","kd_tree"
-#n_neighbors,leaf_size,metric,algorithm=100,100,"minkowski","kd_tree"
-#n_neighbors,leaf_size,metric,algorithm=100,100,"minkowski","brute"
-#n_neighbors,leaf_size,metric,algorithm=200,100,"manhattan","brute"
-n_neighbors,leaf_size,metric,algorithm=100,100,"euclidean","auto"
+classifierVariables = {
+    "firstRow": {"weights": "uniform"},
+    "secondRow": {"weights": "distance"},
+    }
+variables = ["weights"]
 
-
-
-knn = KNeighborsClassifier(n_neighbors,leaf_size=leaf_size,metric=metric,algorithm=algorithm)
-knn.fit(tweet_train, labeled_train)
-
-
-
-
-#evaluate the model
-trainScore = knn.score(tweet_train, labeled_train)
-testScore = knn.score(tweet_test, labeled_test)
-labelPredicted = knn.predict(tweet_test)
-endTime = time.time()
-
-print()
-
-print(f"\nKNN model score on the training dataset: {trainScore:.2f}")
-print(f"KNN model score on the test dataset:     {testScore:.2f}\n")
-
-print(classification_report(labeled_test, labelPredicted, target_names=["Class: 0", "Class: 1"]))
-
-with open(r"Description.txt", "a") as descriptionFile:
-   # descriptionFile.write("="*100)
-    descriptionFile.write(f"\nn_neighbors:       {n_neighbors}")
-    descriptionFile.write(f"\nleaf_size :        {leaf_size}")
-    descriptionFile.write(f"\nmetric:            {metric}")
-    descriptionFile.write(f"\nalgorithm:         {algorithm}")
-    descriptionFile.write(f"\nDimention Reduction:  {len(features.columns)}\n")
-    descriptionFile.write(f"\nDataset used:         {datasetName}\n")
-    descriptionFile.write(f"\nKNN model score on the training dataset:  {trainScore:.2f}")
-    descriptionFile.write(f"\nKNN model score on the test dataset:      {testScore:.2f}")
-    descriptionFile.write(f"\nExecution Time:       {endTime - startTime}s\n\n")
-    descriptionFile.write(classification_report(labeled_test, labelPredicted, target_names=["Class: 0", "Class: 1"])) # type: ignore
-    descriptionFile.write("\n\n")
-    descriptionFile.close()
-
-
-confusionMatrix = confusion_matrix(labeled_test, labelPredicted)
-
-ax= plt.subplot()
-sns.heatmap(confusionMatrix, annot=True, fmt='g', ax=ax, cmap="viridis") # annot=True to annotate cells, ftm='g' to disable scientific notation
-
-# labels, title and ticks
-ax.set_xlabel('Predicted labels')
-ax.set_ylabel('True labels')
-ax.set_title(f"Accuracy: {testScore*100:.2f}%")
-
-ax.xaxis.set_ticklabels(["0", "1"])
-ax.yaxis.set_ticklabels(["0", "1"])
-
-#plt.savefig(f"{datasetName} - {penalty} Penalty - {loss} Loss - {max_iter} max_iter - {len(features.columns)}DR.png", dpi=1000)
-#plt.show()
+MLprocess(cleaned_dataset, padded_docs, columns, classifierVariables, datasetName, variables, startTime)
--- Machine Learning Models/Naive Bayes/GPTDataset Tests/model.py
+++ Machine Learning Models/Naive Bayes/GPTDataset Tests/model.py
@@ -3,7 +3,7 @@ import sys
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
-sys.path.append(r"C:\Users\PC\Documents\GitHub\ArabicSarcasmDetection\sysPath")
+sys.path.append(r"C:\Users\Mohamed\Documents\Fall 2023 - 2024\Senior Project in CS\sysPath")
 os.chdir(dname)
 
 import time
@@ -13,14 +13,14 @@ import seaborn as sns
 import preProcessData #type: ignore
 import matplotlib.pyplot as plt
 
-from keras.preprocessing.text import Tokenizer
 from keras.utils import pad_sequences
+from keras.preprocessing.text import Tokenizer
 
-from sklearn.naive_bayes import GaussianNB
 from sklearn.decomposition import PCA
+from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
+from sklearn.metrics import classification_report, confusion_matrix, precision_score, recall_score, f1_score
 
 from matplotlib import style
 
@@ -34,14 +34,162 @@ if not os.path.isfile(r"Description.txt"):
 
 
 
+def featureEngineering(dataset, padded_docs, columns):
+    labels = dataset["sarcasm"]
+    dataset = dataset.drop(["sarcasm"], axis=1)
+
+    padded_docs = PCA(n_components=len(columns)).fit_transform(padded_docs)
+    padded_docs = StandardScaler().fit_transform(padded_docs)
+    padded_docs = MinMaxScaler().fit_transform(padded_docs)
+
+    features = pd.DataFrame(padded_docs, columns=columns)
+    for column in dataset.columns:
+        features[column] = dataset[column]
+
+    return features, labels
+
+
+
+def modelEvaluation(classifier, train, test):
+    trainScore = classifier.score(train["data"], train["labels"])
+    testScore = classifier.score(test["data"], test["labels"])
+    labelPredicted = classifier.predict(test["data"])
+
+    return trainScore, testScore, labelPredicted
+
+
+def display(dimentionReduction, datasetName, trainScore, testScore, classificationReport, startTime):
+    endTime = time.time()
+    
+    print("="*100)
+    print(f"Dimention Reduction:  {dimentionReduction}")
+    print(f"Dataset used: {datasetName}")
+    print(f"\nSVC model score on the training dataset:  {trainScore:.2f}")
+    print(f"SVC model score on the test dataset:      {testScore:.2f}")
+    print(f"Execution Time:   {abs(endTime - startTime)}s\n")
+    print(classificationReport) # type: ignore
+    print("\n")
+
+
+
+def recordResult(dimentionReduction, datasetName, trainScore, testScore, classificationReport, startTime):
+    endTime = time.time()
+    
+    with open(r"Description.txt", "a") as descriptionFile:
+        descriptionFile.write("="*100)
+        descriptionFile.close()
+
+    with open(r"Description.txt", "a") as descriptionFile:
+        descriptionFile.write(f"\nDimention Reduction:  {dimentionReduction}")
+        descriptionFile.write(f"\nDataset used: {datasetName}\n")
+        descriptionFile.write(f"\nSVC model score on the training dataset:  {trainScore:.2f}")
+        descriptionFile.write(f"\nSVC model score on the test dataset:      {testScore:.2f}")
+        descriptionFile.write(f"\nExecution Time:   {abs(endTime - startTime)}s\n\n")
+        descriptionFile.write(classificationReport) # type: ignore
+        descriptionFile.write("\n\n")
+        descriptionFile.close()
+
+
+
+def saveFig(datasetName, DR, true, predicted, testScore):
+    confusionMatrix = confusion_matrix(true, predicted)
+    ax= plt.subplot()
+    sns.heatmap(confusionMatrix, annot=True, fmt='g', ax=ax, cmap="viridis") # annot=True to annotate cells, ftm='g' to disable scientific notation
+
+    # labels, title and ticks
+    ax.set_xlabel('Predicted labels')
+    ax.set_ylabel('True labels')
+    ax.set_title(f"Accuracy: {testScore*100:.2f}%")
+
+    ax.xaxis.set_ticklabels(["0", "1"])
+    ax.yaxis.set_ticklabels(["0", "1"])
+
+    plt.savefig(f"{datasetName} - {len(DR)}DR.png", dpi=1000)
+    plt.close()
+
+
+
+def recordXLSX(data, DR, precision, recall, f1, testScore):
+    data.loc[len(data.index)] = [int(DR), precision, recall, f1, testScore] # type: ignore
+
+
+
+def barBlot(datasetName, data):    
+    names = data["DR"].tolist()
+
+    f1 = data["F1-score"]
+    accuracy = data["Accuracy"]
+    measures = pd.concat([f1, accuracy], axis=1, join='inner')
+    
+    x = np.arange(len(names))  # the label locations
+    width = 0.25  # the width of the bars
+    multiplier = 0
+
+    fig, ax = plt.subplots(figsize =(15, 10))
+
+    for attribute, measurement in measures.items():
+        offset = width * multiplier
+        rects = ax.bar(x + offset, measurement, width, label=attribute)
+        ax.bar_label(rects, padding=3)
+        multiplier += 1
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel("percentage (%)")
+    ax.set_xlabel(f"Variables: Dimention Reduction")
+    ax.set_title('visualize the Impact of variables')
+
+    ax.set_xticks(x + (width/2), names)
+
+    ax.legend(loc="upper left", ncols=3)
+    ax.set_ylim(0, 1)
+
+    plt.savefig(f"{datasetName}DR.png", dpi=1000)
+    plt.close()
+
+
+
+def MLprocess(dataset, padded_docs, columns, datasetName, Ctime):
+    dataset = dataset.drop(["tweet"], axis=1)
+    data = pd.DataFrame(columns=["DR", "Precision-score", "Recall-score", "F1-score", "Accuracy"])
+    startTime = time.time() - Ctime
+
+    for column in columns:
+        preTime = time.time() + startTime
+
+        features, labels = featureEngineering(dataset, padded_docs, columns[column])
+        tweet_train, tweet_test, labeled_train, labeled_test = train_test_split(features.to_numpy(), labels.to_numpy(), test_size=0.2, shuffle=True)
+        
+        naive_bayes = GaussianNB()
+        naive_bayes.fit(tweet_train, labeled_train)
+        test = {"data": tweet_test, "labels": labeled_test}
+        train = {"data": tweet_train, "labels": labeled_train}
+
+        trainScore, testScore, labelPredicted = modelEvaluation(naive_bayes, train, test)
+        classificationReport = classification_report(labeled_test, labelPredicted, target_names=["Class: 0", "Class: 1"])
+
+        display(len(features.columns), datasetName, trainScore, testScore, classificationReport, preTime)
+        recordResult(len(features.columns), datasetName, trainScore, testScore, classificationReport, preTime)
+        saveFig(datasetName, features.columns, labeled_test, labelPredicted, testScore)
+
+        f1 = f1_score(labeled_test, labelPredicted)
+        recall = recall_score(labeled_test, labelPredicted)
+        precision = precision_score(labeled_test, labelPredicted)
+        recordXLSX(data, len(features.columns), precision, recall, f1, testScore)
+
+    barBlot(datasetName, data.copy(deep=True))
+
+    data.to_excel("record.xlsx", index=False)
+
+
+
 # dataset, datasetName = pd.read_csv(r"https://raw.githubusercontent.com/iabufarha/ArSarcasm-v2/main/ArSarcasm-v2/training_data.csv"), "Original Dataset"
 dataset, datasetName = pd.read_csv(r"../../../Datasets/GPT Dataset.csv"), "GPT Combined Dataset"
 # dataset, datasetName = pd.read_csv(r"../../../Datasets/full Dataset.csv"), "Full Combined Dataset"
-# dataset, datasetName = pd.read_csv(r"../../Datasets/augmented Dataset.csv"), "Augmented Combined Dataset"
-# dataset, datasetName = pd.read_csv(r"../../Datasets/backtrans Dataset.csv"), "Back Translated Combined Dataset"
-# dataset, datasetName = pd.read_csv(r"../../Datasets/synrep Dataset.csv"), "Synonym Replacement Combined Dataset"
-# dataset, datasetName = pd.read_csv(r"../../Datasets/backGPT Dataset.csv"), "Back Translated & GPT Combined Dataset"
-# dataset, datasetName = pd.read_csv(r"../../Datasets/synGPT Dataset.csv"), "Synonym Replacement & GPT Combined Dataset"
+# dataset, datasetName = pd.read_csv(r"../../../Datasets/augmented Dataset.csv"), "Augmented Combined Dataset"
+# dataset, datasetName = pd.read_csv(r"../../../Datasets/backtrans Dataset.csv"), "Back Translated Combined Dataset"
+# dataset, datasetName = pd.read_csv(r"../../../Datasets/synrep Dataset.csv"), "Synonym Replacement Combined Dataset"
+# dataset, datasetName = pd.read_csv(r"../../../Datasets/backGPT Dataset.csv"), "Back Translated & GPT Combined Dataset"
+# dataset, datasetName = pd.read_csv(r"../../../Datasets/synGPT Dataset.csv"), "Synonym Replacement & GPT Combined Dataset"
 
 dataset.info()
 print(f"\n{dataset.head()}")
@@ -49,7 +197,6 @@ print(f"\n{dataset.head()}")
 
 
 cleaned_dataset = preProcessData.preProcessData(dataset.copy(deep=True))
-# cleaned_dataset.to_excel('cleanedset.xlsx', index=False)
 
 
 
@@ -68,80 +215,13 @@ encoded_docs = T.texts_to_sequences(cleaned_dataset["tweet"].tolist())
 # pad documents to a max length of 4 words
 max_length = len(max(np.array(cleaned_dataset["tweet"]), key=len))
 padded_docs = pad_sequences(encoded_docs, maxlen = max_length, padding = "post")
-print("\npadded_docs:\n\n",padded_docs)
+print("\npadded_docs:\n\n", padded_docs, "\n")
 
 
 
-columns = ["A", "B", "C", "D", "E"]
-# columns = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-# columns = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"]
-padded_docs = PCA(n_components=len(columns)).fit_transform(padded_docs)
-padded_docs = StandardScaler().fit_transform(padded_docs)
-padded_docs = MinMaxScaler().fit_transform(padded_docs)
+columnsA = ["A", "B", "C", "D", "E"]
+columnsB = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+columnsC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"]
+columns = {"columnsA": columnsA, "columnsB": columnsB, "columnsC": columnsC}
 
-features = pd.DataFrame(padded_docs, columns=columns)
-features["sentiment"] = cleaned_dataset[["sentiment"]].copy()
-features["dialect"] = cleaned_dataset[["dialect"]].copy()
-
-labels = cleaned_dataset[["sarcasm"]].copy()
-
-
-
-# splits into traint & test
-tweet_train, tweet_test, labeled_train, labeled_test = train_test_split(features.to_numpy(), labels.to_numpy(), test_size=0.2, shuffle=True)
-
-
-
-# fit the model
-# kernel = "linear"
-# kernel = "poly"
-# kernel = "rbf"
-# kernel = "sigmoid"
-kernel = "Non"
-
-NB = GaussianNB()
-NB.fit(tweet_train, labeled_train)
-
-
-
-#evaluate the model
-trainScore = NB.score(tweet_train, labeled_train)
-testScore = NB.score(tweet_test, labeled_test)
-labelPredicted = NB.predict(tweet_test)
-endTime = time.time()
-
-print()
-
-print(f"\nSVC model score on the training dataset: {trainScore:.2f}")
-print(f"SVC model score on the test dataset:     {testScore:.2f}\n")
-
-print(classification_report(labeled_test, labelPredicted, target_names=["Class: 0", "Class: 1"]))
-
-with open(r"Description.txt", "a") as descriptionFile:
-    descriptionFile.write("="*100)
-    descriptionFile.write(f"\nKernel used:  {kernel}")
-    descriptionFile.write(f"\nDimention Reduction:  {len(features.columns)}")
-    descriptionFile.write(f"\nDataset used: {datasetName}\n")
-    descriptionFile.write(f"\nSVC model score on the training dataset:  {trainScore:.2f}")
-    descriptionFile.write(f"\nSVC model score on the test dataset:      {testScore:.2f}")
-    descriptionFile.write(f"\nExecution Time:   {endTime - startTime}s\n\n")
-    descriptionFile.write(classification_report(labeled_test, labelPredicted, target_names=["Class: 0", "Class: 1"])) # type: ignore
-    descriptionFile.write("\n\n")
-    descriptionFile.close()
-
-
-confusionMatrix = confusion_matrix(labeled_test, labelPredicted)
-
-ax= plt.subplot()
-sns.heatmap(confusionMatrix, annot=True, fmt='g', ax=ax, cmap="viridis") # annot=True to annotate cells, ftm='g' to disable scientific notation
-
-# labels, title and ticks
-ax.set_xlabel('Predicted labels')
-ax.set_ylabel('True labels')
-ax.set_title(f"Accuracy: {testScore*100:.2f}%")
-
-ax.xaxis.set_ticklabels(["0", "1"])
-ax.yaxis.set_ticklabels(["0", "1"])
-
-plt.savefig(f"{datasetName} - {kernel} kernel - {len(features.columns)}DR.png", dpi=1000)
-plt.show()
+MLprocess(cleaned_dataset, padded_docs, columns, datasetName, startTime)
