91cd6d0a29b29211b2deb354841433f37ea6a29f
AbdulhadiAlaraj <abdulhadiaraj@gmail.com>
2023-11-15 19:34:48 +0400

    Arabert Modification

--- Deep Learning/araBert/AraBert.py
+++ Deep Learning/araBert/AraBert.py
@@ -19,6 +19,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
+from keras.utils import plot_model
 
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -48,33 +49,38 @@ train_encoded, train_labels = tokenize_and_preprocess(train_df, tokenizer, prepr
 val_encoded, val_labels = tokenize_and_preprocess(val_df, tokenizer, preprocess)
 test_encoded, test_labels = tokenize_and_preprocess(test_df, tokenizer, preprocess)
 
+BATCH_SIZE = 8
+
 # Create TensorFlow datasets
 train_dataset = tf.data.Dataset.from_tensor_slices((
     {'input_ids': train_encoded['input_ids'], 'attention_mask': train_encoded['attention_mask']},
     train_labels
-)).shuffle(len(train_df)).batch(8)
+)).shuffle(len(train_df)).batch(BATCH_SIZE)
 
 val_dataset = tf.data.Dataset.from_tensor_slices((
     {'input_ids': val_encoded['input_ids'], 'attention_mask': val_encoded['attention_mask']},
     val_labels
-)).batch(8)
+)).batch(BATCH_SIZE)
 
 test_dataset = tf.data.Dataset.from_tensor_slices((
     {'input_ids': test_encoded['input_ids'], 'attention_mask': test_encoded['attention_mask']},
     test_labels
-)).batch(8)
+)).batch(BATCH_SIZE)
 
 
 # Load the AraBERT model with a classification head
 model = TFAutoModelForSequenceClassification.from_pretrained("aubmindlab/bert-base-arabertv02", num_labels=2)
 
+EPOCH = 1
+LEARNING_RATE = 5e-7
+
 # Compile the model
-optimizer = tf.keras.optimizers.Adam(learning_rate=5e-7)
+optimizer = tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE)
 loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
 
 # Train the model with validation data
-history = model.fit(train_dataset, validation_data=val_dataset, epochs=2)
+history = model.fit(train_dataset, validation_data=val_dataset, epochs=EPOCH)
 
 # Evaluate the model on the test dataset
 test_loss, test_accuracy = model.evaluate(test_dataset)
@@ -91,6 +97,7 @@ plt.title('Model Accuracy')
 plt.ylabel('Accuracy')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Val'], loc='upper left')
+#plt.savefig(f"Epoch :{EPOCH}, Learning Rate:{LEARNING_RATE} Training vs validation Accuracy", dpi=1000)
 plt.show()
 
 plt.plot(history.history['loss'])
@@ -99,6 +106,7 @@ plt.title('Model Loss')
 plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Val'], loc='upper left')
+#plt.savefig(f"Epoch :{EPOCH}, Learning Rate:{LEARNING_RATE} Training vs validation loss", dpi=1000)
 plt.show()
 
 # Confusion Matrix
@@ -106,4 +114,5 @@ cm = confusion_matrix(true_classes, predicted_classes)
 sns.heatmap(cm, annot=True, fmt='d')
 plt.ylabel('Actual')
 plt.xlabel('Predicted')
+#plt.savefig(f"Epoch :{EPOCH}, Learning Rate:{LEARNING_RATE} Confusion Matrix", dpi=1000)
 plt.show()
