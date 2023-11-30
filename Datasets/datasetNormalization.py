d8e0c4b2302356867f7147611309e06dc27b540d
Mohamed Erfan Mohamed Elfares <130843116+MohamedElfares@users.noreply.github.com>
2023-11-30 17:15:22 +0400

    .

--- Datasets/datasetNormalization.py
+++ Datasets/datasetNormalization.py
@@ -5,31 +5,26 @@ ArsarcasmTest = pd.read_csv(r"https://raw.githubusercontent.com/iabufarha/ArSarc
 iSarcasmEvalTrain = pd.read_csv(r"https://raw.githubusercontent.com/iabufarha/iSarcasmEval/main/train/train.Ar.csv")[['text','sarcastic', 'dialect']]
 iSarcasmEvalTestA = pd.read_csv(r"https://raw.githubusercontent.com/iabufarha/iSarcasmEval/main/test/task_A_Ar_test.csv")[['text','sarcastic', 'dialect']]
 iSarcasmEvalTestC = pd.read_csv(r"https://raw.githubusercontent.com/iabufarha/iSarcasmEval/main/test/task_C_Ar_test.csv")[['text_0','text_1', 'sarcastic_id', 'dialect']]
-iSarcasmEvalTrain.columns = ['tweet', 'sarcasm', 'dialect']
 
+
+
+iSarcasmEvalTrain.columns = ['tweet', 'sarcasm', 'dialect']
 iSarcasmEvalTestC['sarcastic_tweet'] = iSarcasmEvalTestC.apply(lambda row: row['text_1'] if row['sarcastic_id'] == 1 else row['text_0'], axis=1)
 iSarcasmEvalTestC['non_sarcastic_tweet'] = iSarcasmEvalTestC.apply(lambda row: row['text_0'] if row['sarcastic_id'] == 1 else row['text_1'], axis=1)
-sarcastic_df = pd.DataFrame({
-    'tweet': iSarcasmEvalTestC['sarcastic_tweet'],
-    'sarcasm': 1,
-    'dialect': iSarcasmEvalTestC['dialect']
-})
-# Create a DataFrame for non-sarcastic tweets
-non_sarcastic_df = pd.DataFrame({
-    'tweet': iSarcasmEvalTestC['non_sarcastic_tweet'],
-    'sarcasm': 0,
-    'dialect': iSarcasmEvalTestC['dialect']
-})
-# Combine the DataFrames
+
+sarcastic_df = pd.DataFrame({'tweet': iSarcasmEvalTestC['sarcastic_tweet'], 'sarcasm': 1, 'dialect': iSarcasmEvalTestC['dialect']})
+non_sarcastic_df = pd.DataFrame({'tweet': iSarcasmEvalTestC['non_sarcastic_tweet'], 'sarcasm': 0, 'dialect': iSarcasmEvalTestC['dialect']})
 iSarcasmEvalTestC = pd.concat([sarcastic_df, non_sarcastic_df], ignore_index=True)
+
+
+
 # Combine all three DataFrames
 dataset = pd.concat([ArsarcasmTrain, ArsarcasmTest, iSarcasmEvalTrain, iSarcasmEvalTestC], ignore_index=True)
-dataset.drop_duplicates()
-dataset.dropna
-print(dataset.sample(frac=1))
-# Assuming combined_df is your combined DataFrame
-sarcastic_counts = dataset['sarcasm'].value_counts()
+dataset = dataset.drop_duplicates()
+dataset = dataset.dropna()
+dataset = dataset.sample(frac=1)
+
 # Print the counts
-print(sarcastic_counts)
+print(dataset['sarcasm'].value_counts())
 
 dataset.to_csv(rf"Datasets\originalCombined.csv", index=False)
