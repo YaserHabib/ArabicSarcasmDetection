b16eafd7d92ba08419dcadd1da8427cb2f5f7f8e
Mohamed Erfan Mohamed Elfares <130843116+MohamedElfares@users.noreply.github.com>
2023-12-04 05:24:10 +0400

    update CNN

--- Deep Learning/CNN/Model.py
+++ Deep Learning/CNN/Model.py
@@ -2,19 +2,21 @@ import os
 import sys
 import time
 
-from sympy import rational_interpolate
-
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 sys.path.append(r"../../sysPath")
 os.chdir(dname)
 
+models_dir = os.path.join(dname, 'Trained Models [CNN]')
+if not os.path.exists(models_dir):
+    os.makedirs(models_dir)
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Set TensorFlow logging level to 2 (ERROR)
 
+import pickle
 import shutil
 import warnings
 import pandas as pd
-import preProcessData #type: ignore
 import tensorflow as tf
 
 from summarize import prepareData, summarize, TrainTestSplit, smote, fit, modelEvaluation, display # type: ignore
@@ -38,7 +40,7 @@ def configModel(max_length, vocab_size, TOTAL_EMBEDDING_DIM, embedding_matrix):
         tf.keras.layers.Embedding(vocab_size, TOTAL_EMBEDDING_DIM, weights = [embedding_matrix] , input_length = max_length, trainable = True),
 
         # Conv1D layer for pattern recognition model and extract the feature from the vectors
-        tf.keras.layers.Conv1D(filters = 128, kernel_size = 3, activation = "relu"),
+        tf.keras.layers.Conv1D(filters = 64, kernel_size = 3, activation = "relu"),
 
         tf.keras.layers.BatchNormalization(),
 
@@ -58,24 +60,40 @@ def configModel(max_length, vocab_size, TOTAL_EMBEDDING_DIM, embedding_matrix):
     ])
 
     # compile the model
-    model.compile(optimizer = tf.keras.optimizers.Adam(0.00002), metrics = ["accuracy"], loss = "binary_crossentropy")
+    # LEARNING_RATE = 0.00002
+    model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate = 0.00005), metrics = ["accuracy"], loss = "binary_crossentropy")
 
     return model
 
 
 
-ArsarcasmTrain = pd.read_csv(r"https://raw.githubusercontent.com/iabufarha/ArSarcasm-v2/main/ArSarcasm-v2/training_data.csv")[['tweet','sarcasm', "dialect"]]
+ArsarcasmTrain = pd.read_csv(r"https://raw.githubusercontent.com/iabufarha/ArSarcasm-v2/main/ArSarcasm-v2/training_data.csv")[["tweet", "dialect", "sarcasm"]]
+ArsarcasmTest = pd.read_csv(r"https://raw.githubusercontent.com/iabufarha/ArSarcasm-v2/main/ArSarcasm-v2/testing_data.csv")[["tweet", "dialect", "sarcasm"]]
 
-iSarcasmEvalTrain = pd.read_csv(r"https://raw.githubusercontent.com/iabufarha/iSarcasmEval/main/train/train.Ar.csv")[['text','sarcastic', 'dialect']]
-iSarcasmEvalTrain.columns = ['tweet','sarcasm', "dialect"]
+iSarcasmEvalTrain = pd.read_csv(r"https://raw.githubusercontent.com/iabufarha/iSarcasmEval/main/train/train.Ar.csv")[["text","dialect", "sarcastic"]]
+iSarcasmEvalTrain_rephrase = pd.read_csv(r"https://raw.githubusercontent.com/iabufarha/iSarcasmEval/main/train/train.Ar.csv")[["rephrase", "dialect", "sarcastic"]]
+iSarcasmEvalTestA = pd.read_csv(r"https://raw.githubusercontent.com/iabufarha/iSarcasmEval/main/test/task_A_Ar_test.csv")[["text", "dialect", "sarcastic"]]
+iSarcasmEvalTestC = pd.read_csv(r"https://raw.githubusercontent.com/iabufarha/iSarcasmEval/main/test/task_C_Ar_test.csv")[["text_0", "text_1", "dialect", "sarcastic_id"]]
 
-iEval_Ar_Dataset = pd.concat([ArsarcasmTrain, iSarcasmEvalTrain], ignore_index=True)
-fullDataset = pd.read_csv(r"../../Datasets/full Dataset.csv")[['tweet','sarcasm', "dialect"]]
+iSarcasmEvalTrain.columns = ["tweet", "dialect", "sarcasm"]
+iSarcasmEvalTrain_rephrase.columns = ["tweet", "dialect", "sarcasm"]
+iSarcasmEvalTestA.columns = ["tweet", "dialect", "sarcasm"]
+
+iSarcasmEvalTestC["sarcastic_tweet"] = iSarcasmEvalTestC.apply(lambda row: row["text_1"] if row["sarcastic_id"] == 1 else row["text_0"], axis=1)
+iSarcasmEvalTestC["non_sarcastic_tweet"] = iSarcasmEvalTestC.apply(lambda row: row["text_0"] if row["sarcastic_id"] == 1 else row["text_1"], axis=1)
+sarcastic_df = pd.DataFrame({"tweet": iSarcasmEvalTestC["sarcastic_tweet"], "dialect": iSarcasmEvalTestC["dialect"], "sarcasm": 1})
+non_sarcastic_df = pd.DataFrame({"tweet": iSarcasmEvalTestC["non_sarcastic_tweet"], "dialect": iSarcasmEvalTestC["dialect"], "sarcasm": 0})
+iSarcasmEvalTestC = pd.concat([sarcastic_df, non_sarcastic_df], ignore_index=True)
+
+Arsarcasm = pd.concat([ArsarcasmTrain, ArsarcasmTest], ignore_index = True)
+iSarcasmEval = pd.concat([iSarcasmEvalTrain, iSarcasmEvalTrain_rephrase, iSarcasmEvalTestA, iSarcasmEvalTestC], ignore_index = True)
+ARISarcasm = pd.concat([Arsarcasm, iSarcasmEval], ignore_index = True)[["tweet","dialect", "sarcasm"]]
+fullDataset = pd.read_csv(r"../../Datasets/full Dataset.csv")[["tweet","dialect", "sarcasm"]]
 
 datasets = {
     "ArsarcasmV2 Dataset": ArsarcasmTrain,
-    "ISarcasmEval Dataset": iSarcasmEvalTrain,
-    "IEval_Ar_Dataset": iEval_Ar_Dataset,
+    "iSarcasmEval Dataset": iSarcasmEvalTrain,
+    "ARISarcasm": ARISarcasm,
     "Full Combined Dataset": fullDataset
 }
 
@@ -87,7 +105,9 @@ smoteControl = False
 for datasetName, dataset in datasets.items():
     start = time.time()
     ratio = dataset["sarcasm"].value_counts()[1] / len(dataset)
-    smoteStatus = True if ratio > 0.40 and smoteControl else False
+    model_path = os.path.join(models_dir, f"{datasetName}.pkl")
+
+    smoteStatus = True if ( 0.40 > ratio or ratio > 0.60) and smoteControl else False
 
     dataset, max_length, vocab_size, TOTAL_EMBEDDING_DIM, embedding_matrix, padded_docs = prepareData(dataset)
 
@@ -99,9 +119,12 @@ for datasetName, dataset in datasets.items():
 
     train_tweet, train_labels = smote(train_tweet, train_labels) if smoteStatus else (train_tweet, train_labels)
 
-    result = fit(model, train_labels, train_tweet, val_tweet, val_labels)
+    result = fit(model, train_labels, train_tweet, val_tweet, val_labels, 15)
 
     predicted, precision, accuracy, recall, f1, classificationReport = modelEvaluation(model, test_tweet, test_labels)
+
+    with open(model_path, "wb") as file:
+        pickle.dump(model, file)
 
     end = time.time()
 
@@ -111,7 +134,7 @@ for datasetName, dataset in datasets.items():
 
     plotPerformance(result, datasetName)
 
-    saveFig(test_labels, predicted, accuracy, datasetName)
+    saveFig(test_labels, predicted, accuracy, "CNN", datasetName)
 
     nonSarcasmCount = dataset["sarcasm"].value_counts()[0]
     sarcasmCount = dataset["sarcasm"].value_counts()[1]
@@ -119,4 +142,5 @@ for datasetName, dataset in datasets.items():
 
 
 barPlot(data, "CNN")
-data.to_excel("model performance - SMOTE ON.xlsx", index = False)
+recordName = "model performance - SMOTE ON.xlsx" if smoteControl else "model performance.xlsx"
+data.to_excel(recordName, index = False)
